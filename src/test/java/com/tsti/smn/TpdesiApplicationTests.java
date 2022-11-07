0260b37ff94625c7fa55d5801d3b1ef631acf0ac
nico9 <nico9@host.docker.internal>
2022-11-07 17:57:07 -0300

    Inicializacion de proyecto.

---
+++ src/test/java/com/tsti/smn/TpdesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsti.smn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpdesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
